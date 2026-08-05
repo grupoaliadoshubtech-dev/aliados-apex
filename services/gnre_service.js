af6e7a544a9c3ccc271cd6bc0da52203398ec881
jrobertodasc-cmd <jrobertodasc@gmail.com>
2026-08-05 10:10:10 -0300

    Update gnre_service.js

--- services/gnre_service.js
+++ services/gnre_service.js
@@ -68,7 +68,7 @@ function sendSoapRequest(urlStr, soapAction, xmlPayload, agent) {
             path: url.pathname + url.search,
             port: url.port || 443,
             agent: agent,
-            timeout: 15000, // Timeout de 15 segundos
+            timeout: 60000, // Timeout de 15 segundos
             headers: {
                 'Content-Type': 'application/soap+xml; charset=utf-8;',
                 'SOAPAction': soapAction,
